8a39710678b12f36c1701229cac2397369287b1f
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-12-31 11:18:30 +0100

    fix: adding spanish resources files

---
+++ P2FixAnAppDotNetCode/Resources/Models/ViewModels/Resources.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace P2FixAnAppDotNetCode.Resources.Models.ViewModels
+{
+    public static class Order
+    {
+        private static ResourceManager resourceManager = new ResourceManager("P2FixAnAppDotNetCode.Resources.Models.ViewModels.Order", Assembly.GetExecutingAssembly());
+        private static CultureInfo resourceCulture;
+
+        public static string ErrorMissingName
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingName", resourceCulture);
+            }
+        }
+        public static string ErrorMissingAddress
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingAddress", resourceCulture);
+            }
+        }
+        public static string ErrorMissingCity
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingCity", resourceCulture);
+            }
+        }
+        public static string ErrorMissingCountry
+        {
+            get
+            {
+                return resourceManager.GetString("ErrorMissingCountry", resourceCulture);
+            }
+        }
+    }
+}
+
